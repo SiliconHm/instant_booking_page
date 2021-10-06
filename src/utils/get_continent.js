6eaaf9d0a2896ff182df7ea978e597c0af7cb9f9
azam-tanoli <azamtanoli00@gmail.com>
2021-10-06 20:58:18 +0500

    get continent of user

--- src/utils/get_continent.js
+++ src/utils/get_continent.js
@@ -2,15 +2,15 @@ import publicIP from 'react-native-public-ip';
 
 const GetContinent = async () => {
     // const [region, setRegion] = useState({})
-    const ACCESS_KEY = '40dfac267f618003e5700ee90f912365';
+    const key = process.env.REACT_ACCESS_KEY;
     const publicIpAddress = await publicIP();
-    const url = `http://api.ipstack.com/${publicIpAddress}?access_key=${ACCESS_KEY}`
+    const url = `http://api.ipstack.com/${publicIpAddress}?access_key=${key}`
     return fetch(url, {
             method: 'GET'
         })
         .then(res => {
             return res.json();
-        })
+        }) 
         .catch(err => console.log(err))
 }
 
